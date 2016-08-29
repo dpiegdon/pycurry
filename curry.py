6b45eb1929827d71b39ac70dc33192d597255eaf
David R. Piegdon <dgit@piegdon.de>
2016-08-29 23:58:11 +0000

    formalized license as MIT

--- curry.py
+++ curry.py
@@ -48,9 +48,27 @@ def curry(lazy = True, allow_override = False, use_defaults = False):
 		* David R. Piegdon
 
 	License:
-		You can do whatever you want with this stuff.
-		If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
-		The Author(s).
+		The MIT License (MIT)
+		Copyright (c) 2015, 2016 David R. Piegdon
+
+		Permission is hereby granted, free of charge, to any person obtaining a
+		copy of this software and associated documentation files (the
+		"Software"), to deal in the Software without restriction, including
+		without limitation the rights to use, copy, modify, merge, publish,
+		distribute, sublicense, and/or sell copies of the Software, and to
+		permit persons to whom the Software is furnished to do so, subject to
+		the following conditions:
+
+		The above copyright notice and this permission notice shall be included
+		in all copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+		OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+		MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+		IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+		CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+		TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+		SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 	Prior art and inspiration:
 		* functional programming, haskell
@@ -61,6 +79,7 @@ def curry(lazy = True, allow_override = False, use_defaults = False):
 
 	ChangeLog:
 		2015-04-11 - initial implementation and testsuite
+		2016-08-29 - documentation and license cleanup for release on github
 	"""
 
 	if not ((type(lazy) is bool) and (type(allow_override) is bool) and (type(use_defaults) is bool)):
